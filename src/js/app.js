97b56050e5ac2c3ce4c7f59a6116608ae73de129
Vladimir Ostapov <v.ttbomk@gmail.com>
2020-01-02 22:57:11 +0300

    Завершил адаптивную разметку, затрачено 2 часа

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-// import SimpleParallax from 'simple-parallax-js';
+import SimpleParallax from 'simple-parallax-js';
 import 'slick-carousel';
 import "./menu";
 
@@ -6,12 +6,12 @@ document.addEventListener('DOMContentLoaded', function(){
   const $welcomeSlider = $(".welcome__items");
 
   $welcomeSlider.on('init', function(event, slick, currentSlide, nextSlide) {
-    // const parallaxElement = document.querySelectorAll('.js-parallax');
-    // const parallax = new SimpleParallax(parallaxElement, {
-    //   overflow: true,
-    //   delay: .25,
-    //   transition: 'cubic-bezier(0,0,0,1)'
-    // });
+    const parallaxElement = document.querySelectorAll('.js-parallax');
+    const parallax = new SimpleParallax(parallaxElement, {
+      overflow: true,
+      delay: .25,
+      transition: 'cubic-bezier(0,0,0,1)'
+    });
   });
 
   $welcomeSlider.slick({
@@ -25,26 +25,51 @@ document.addEventListener('DOMContentLoaded', function(){
     // autoplaySpeed: 1000
   });
 
-  // const modal = document.querySelectorAll('.modal-img');
-  // const modalParallax = new SimpleParallax(modal, {
-  //   overflow: true,
-  //   delay: .5
-  // });
 
   $(".service__slider-items").slick({
     infinite: false,
     dots: false,
+    arrows: true,
     speed: 300,
     slidesToShow: 1,
     variableWidth: true,
+    prevArrow: $('.service__button.js-prev'),
+    nextArrow: $('.service__button.js-next'),
     responsive: [
       {
         breakpoint: 850,
         settings: {
           slidesToShow: 1,
-          variableWidth: false
+          variableWidth: false,
+          fade: true
         }
       }
     ]
+  });
+
+  $(".review__items").slick({
+    infinite: true,
+    dots: false,
+    fade: true,
+    cssEase: 'linear',
+    speed: 500,
+    slidesToShow: 1,
+    arrows: true,
+    prevArrow: $('.review__button.js-prev'),
+    nextArrow: $('.review__button.js-next')
+  });
+
+  const parallaxOne = document.querySelectorAll('.js-parallax.js-parallax_img-1');
+  const oneParallax = new SimpleParallax(parallaxOne, {
+    overflow: true,
+    delay: 1.5,
+    transition: 'cubic-bezier(0.68, -0.55, 0.265, 1.5)'
+  });
+
+  const parallaxTwo = document.querySelectorAll('.js-parallax.js-parallax_img-2');
+  const twoParallax = new SimpleParallax(parallaxTwo, {
+    overflow: true,
+    delay: .6,
+    transition: 'cubic-bezier(0.645, 0.045, 0.355, 1)'
   });
 });
